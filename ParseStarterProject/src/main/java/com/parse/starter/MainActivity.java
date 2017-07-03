1cdca1a58a678119e8b80a6b775a3d774c431dea
codeshef <mukulanchal@gmail.com>
2017-07-03 20:25:40 +0530

    Adding  advance keyboard features

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,9 +11,14 @@ package com.parse.starter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,7 +37,14 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
+
+    EditText  usernameEditText;
+    EditText  passwordEditText;
+    TextView  changeSignUpModeTextView;
+    RelativeLayout backgroundView;
+    ImageView  instaLogo;
+
 
     Boolean signUpModeActive = true;
     Button signUpButton;
@@ -56,12 +68,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                changeSignUpModeTextView.setText("Or,LogIn");
 
            }
+       }else if(view.getId() == R.id.backgroundView || view.getId() == R.id.instaLogo){
+
+           InputMethodManager inputMethodManager =(InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+           inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+
        }
     }
 
-    EditText  usernameEditText;
-    EditText  passwordEditText;
-    TextView  changeSignUpModeTextView;
+
 
     public void signUp(View view){
 
@@ -114,14 +129,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-      usernameEditText = (EditText) findViewById(R.id.usernameEditText);
-      passwordEditText = (EditText) findViewById(R.id.passwordEditText);
-      changeSignUpModeTextView =(TextView) findViewById(R.id.changeSignUpModeTextView);
-      signUpButton = (Button) findViewById(R.id.signUpButton);
+       usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+       passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+       changeSignUpModeTextView =(TextView) findViewById(R.id.changeSignUpModeTextView);
+       signUpButton = (Button) findViewById(R.id.signUpButton);
+       backgroundView =(RelativeLayout)  findViewById(R.id.backgroundView);
+          instaLogo = (ImageView)  findViewById(R.id.instaLogo);
 
       changeSignUpModeTextView.setOnClickListener(this);
 
-              ParseAnalytics.trackAppOpenedInBackground(getIntent());
+         //backgroundView.setOnClickListener(this);
+
+         instaLogo.setOnClickListener(this);
+
+
+
+      passwordEditText.setOnKeyListener(this);
+
+      ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
 
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+        if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == event.ACTION_DOWN){
+
+            signUp(v);
+        }
+
+        return false;
+    }
 }
